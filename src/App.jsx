e0c682d45502d8d3300357100d02d7e0f789fba6
romainniamor <romainnavoret@gmail.com>
2023-12-17 18:15:51 +0100

    add button

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+import ButtonLink from "./components/ButtonLink";
+
+function App() {
+  return (
+    <>
+      <nav className="fixed top-0 left-0 w-full flex justify-end p-12">
+        <div className="flex flex-col gap-4">
+          <ButtonLink text="Home" />
+          <ButtonLink text="About" />
+          <ButtonLink text="Works" />
+          <ButtonLink text="Contact" />
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ButtonLink.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from "react";
+
+import gsap from "gsap";
+
+function ButtonLink({ text }) {
+  const ref1 = useRef();
+  const ref2 = useRef();
+
+  const onHover = () => {
+    // Animation GSAP pour les deux références
+    gsap.to([ref1.current, ref2.current], { y: "-100%", duration: 0.3 });
+  };
+
+  const onLeave = () => {
+    // Animation GSAP pour revenir à la position originale
+    gsap.to([ref1.current, ref2.current], { y: 0, duration: 0.3 });
+  };
+
+  return (
+    <a
+      href=""
+      onMouseEnter={onHover}
+      onMouseLeave={onLeave}
+      className="menu-item relative text-xl font-bold leading-none overflow-hidden cursor-pointer"
+    >
+      <div ref={ref1} className=" text-neutral-300/20 f text-right">
+        {text}
+      </div>
+      <div className="hover-clone absolute bottom-[-100%] right-0  text-neutral-300 text-right">
+        <div ref={ref2}>{text}</div>
+      </div>
+    </a>
+  );
+}
+
+export default ButtonLink;

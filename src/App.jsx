a72074c4ec23bb60b0b7a70799eac34e3ba13980
romainniamor <romainnavoret@gmail.com>
2023-12-18 00:06:01 +0100

    cursor done need to manage templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
 import ButtonLink from "./components/ButtonLink";
-import MouseMove from "./components/MouseMove";
 
 function App() {
-  return (
-    <>
-      <MouseMove />
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [cursorVariant, setCursorVariant] = useState(false);
 
-      <nav className="fixed top-0 left-0 w-full flex justify-end p-12">
+  // Log the current mouse position
+  console.log("mousePosition:", mousePosition);
+
+  useEffect(() => {
+    // Define the mouseMove handler
+    const mouseMove = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    // Add event listener for mouse movement
+    window.addEventListener("mousemove", mouseMove);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, [mousePosition]); // Empty dependency array to run only once
+
+  const textEnter = () => {
+    console.log("textEnter");
+    setCursorVariant(true);
+  };
+  const textLeave = () => {
+    console.log("textLeave");
+    setCursorVariant(false);
+  };
+
+  return (
+    <div className="app w-full h-screen relative bg-black flex flex-col justify-center items-center">
+      <div
+        style={{
+          left: `${mousePosition.x}px`,
+          top: `${mousePosition.y}px`,
+          transform: `translate(-50%, -50%) ${
+            cursorVariant ? "scale(15)" : "scale(1)"
+          }`,
+          mixBlendMode: cursorVariant ? "difference" : "normal",
+          content: cursorVariant ? "Bye world" : "none",
+        }}
+        className="cursor fixed top-0 left-0 bg-neutral-300 rounded-full w-5 h-5 transition-all duration-300 ease-out pointer-events-none"
+      ></div>
+      <nav className="fixed top-0 left-0 w-full flex justify-end z-10">
         <div className="flex flex-col gap-4">
           <ButtonLink text="Home" />
           <ButtonLink text="About" />
@@ -16,7 +54,14 @@ function App() {
           <ButtonLink text="Contact" />
         </div>
       </nav>
-    </>
+      <h1
+        onMouseEnter={textEnter}
+        onMouseLeave={textLeave}
+        className="text-neutral-300 text-9xl"
+      >
+        Hello World
+      </h1>
+    </div>
   );
 }
 
--- src/components/MouseMove.jsx
+++ src/components/MouseMove.jsx
@@ -1,35 +1,21 @@
 import React, { useState, useEffect } from "react";
 
-function MouseMove({ handleMouseHover, handleMouseLeave, handleMouseHidden }) {
+function MouseMove({ isMouseHover, isVisible }) {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [isMouseHover, setIsMouseHover] = useState(false);
-  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     const handleMouseMove = (e) => {
       setMousePosition({ x: e.pageX, y: e.pageY });
     };
-
-    //logique pour modifier le pointer dans le parent
-
-    const handleMouseHidden = () => {
-      setIsVisible(false);
-    };
-
-    const handleMouseHover = () => {
-      setIsMouseDown(true);
-    };
-
-    const handleMouseLeave = () => {
-      setIsMouseDown(false);
-    };
+    console.log("isMouseHover:", isMouseHover);
+    console.log("mousePosition:", mousePosition);
 
     window.addEventListener("mousemove", handleMouseMove);
 
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
-  }, []);
+  }, [mousePosition]);
 
   return (
     isVisible && (
@@ -37,7 +23,9 @@ function MouseMove({ handleMouseHover, handleMouseLeave, handleMouseHidden }) {
         style={{
           left: `${mousePosition.x}px`,
           top: `${mousePosition.y}px`,
-          transform: isMouseHover ? "scale(25)" : "scale(1)",
+          transform: `translate(-50%, -50%) ${
+            isMouseHover ? "scale(20)" : "scale(1)"
+          }`,
         }}
         className="absolute w-5 h-5 rounded-full bg-neutral-300 transition-all duration-100 ease-out"
       ></div>

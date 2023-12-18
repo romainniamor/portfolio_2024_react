ff5edd5045d0057fabade978f4b5d686fe9ade15
romainniamor <romainnavoret@gmail.com>
2023-12-18 13:44:14 +0100

    header anim h1 ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,85 @@
 import { useState, useEffect } from "react";
 import ButtonLink from "./components/ButtonLink";
+import { motion } from "framer-motion";
 
 function App() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [cursorVariant, setCursorVariant] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
 
-  // Log the current mouse position
-  console.log("mousePosition:", mousePosition);
+  const size = () => {
+    if (!isVisible) return 1;
+    if (isHovered) return 500;
+    return 1;
+  };
+
+  // console.log("mousePosition:", mousePosition);
 
   useEffect(() => {
-    // Define the mouseMove handler
-    const mouseMove = (e) => {
+    const updateMousePosition = (e) => {
       setMousePosition({ x: e.clientX, y: e.clientY });
     };
 
     // Add event listener for mouse movement
-    window.addEventListener("mousemove", mouseMove);
+    window.addEventListener("mousemove", updateMousePosition);
 
     // Cleanup function to remove the event listener
     return () => {
-      window.removeEventListener("mousemove", mouseMove);
+      window.removeEventListener("mousemove", updateMousePosition);
     };
-  }, [mousePosition]); // Empty dependency array to run only once
+  }, [mousePosition]);
 
   const textEnter = () => {
-    console.log("textEnter");
-    setCursorVariant(true);
+    setIsHovered(true);
   };
   const textLeave = () => {
-    console.log("textLeave");
-    setCursorVariant(false);
+    setIsHovered(false);
+  };
+  const linkEnter = () => {
+    setIsVisible(false);
+    console.log("enter");
+  };
+  const linkLeave = () => {
+    setIsVisible(true);
   };
 
   return (
     <div className="app w-full h-screen relative bg-black flex flex-col justify-center items-center">
-      <div
-        style={{
-          left: `${mousePosition.x}px`,
-          top: `${mousePosition.y}px`,
-          transform: `translate(-50%, -50%) ${
-            cursorVariant ? "scale(15)" : "scale(1)"
-          }`,
-          mixBlendMode: cursorVariant ? "difference" : "normal",
-          content: cursorVariant ? "Bye world" : "none",
-        }}
-        className="cursor fixed top-0 left-0 bg-neutral-300 rounded-full w-5 h-5 transition-all duration-300 ease-out pointer-events-none"
-      ></div>
-      <nav className="fixed top-0 left-0 w-full flex justify-end z-10">
-        <div className="flex flex-col gap-4">
+      <nav className="fixed top-0 left-0 w-full flex justify-end z-20 ">
+        <div
+          onMouseEnter={linkEnter}
+          onMouseLeave={linkLeave}
+          className="flex flex-col gap-4 p-9"
+        >
           <ButtonLink text="Home" />
           <ButtonLink text="About" />
           <ButtonLink text="Works" />
           <ButtonLink text="Contact" />
         </div>
       </nav>
-      <h1
-        onMouseEnter={textEnter}
-        onMouseLeave={textLeave}
-        className="text-neutral-300 text-9xl"
+      <h1 className="text-neutral-300 text-9xl">Hello World</h1>
+      <motion.div
+        className="mask absolute w-full h-full flex justify-center items-center"
+        style={{
+          WebkitMaskPosition: `${mousePosition.x}px ${mousePosition.y}px`,
+          WebkitMaskSize: `${size()}px`,
+        }}
+        animate={{
+          WebkitMaskPosition: `${mousePosition.x - size() / 2}px ${
+            mousePosition.y - size() / 2
+          }px`,
+          WebkitMaskSize: `${size()}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut", duration: 0.3 }}
       >
-        Hello World
-      </h1>
+        <h1
+          className="text-9xl"
+          onMouseEnter={textEnter}
+          onMouseLeave={textLeave}
+        >
+          Bye World
+        </h1>
+      </motion.div>
     </div>
   );
 }

b81e0a1a892f307d575cc9314f9fb99df9455a90
romainniamor <romainnavoret@gmail.com>
2023-12-18 18:43:17 +0100

    svg link done

--- src/components/MouseMove.jsx
+++ src/components/MouseMove.jsx
@@ -1,36 +1,19 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-function MouseMove({ isMouseHover, isVisible }) {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+
+  const updateMousePosition = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
 
   useEffect(() => {
-    const handleMouseMove = (e) => {
-      setMousePosition({ x: e.pageX, y: e.pageY });
-    };
-    console.log("isMouseHover:", isMouseHover);
-    console.log("mousePosition:", mousePosition);
+    window.addEventListener("mousemove", updateMousePosition);
 
-    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", updateMousePosition);
+  }, []);
 
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, [mousePosition]);
+  return mousePosition;
+};
 
-  return (
-    isVisible && (
-      <div
-        style={{
-          left: `${mousePosition.x}px`,
-          top: `${mousePosition.y}px`,
-          transform: `translate(-50%, -50%) ${
-            isMouseHover ? "scale(20)" : "scale(1)"
-          }`,
-        }}
-        className="absolute w-5 h-5 rounded-full bg-neutral-300 transition-all duration-100 ease-out"
-      ></div>
-    )
-  );
-}
-
-export default MouseMove;
+export default useMousePosition;
